a458fea6110efced200b40d1e6912f913f025386
Abayon Greg <gregabayon23@gmail.com>
2025-03-28 16:36:29 +0800

    Update program5.cxx

--- program5.cxx
+++ program5.cxx
@@ -1,41 +1,64 @@
 #include <iostream>
-using namespace std;
 
-int main(){
-	// declaring variable without assignibg value
-	string name;
-	int num1, num2, result;
-	
-	// asking user to input name
-	cout<<" What's your name? ";
-	cin>>name;
-	cout << "Hello " << name<< "!" << endl;
-	
-	// asking user to input two nymbers
-	cout << " Enter two numbers: ";
-	cin>> num1 >>num2;
-	
-	// addition of num1 and num2
-	result = num1 + num2;
-	cout << " The sum of two numbers: " << result << endl;
-	
-	// subraction of num1 and num2
-	result = num1 - num2;
-	cout << " The difference of two numbers: " << result << endl;
-	
-	// multiplication of num1 and nym2
-	result = num1 * num2;
-	cout << " The product of two numbers: " << result << endl;
-	
-	// division of num1 and num2
-	result = num1 / num2;
-	cout << " The quotient of two numbers: " << result << endl;
-	
-	// remainder of num1 and num2
-	result = num1 % num2;
-	cout << " The modulus of two numbers: " << result << endl;
-	
-	
-	
-		return 0;
-	}
+// a function that ask user to input name
+void name() {
+    std::string name;
+    std::cout << "What's your name? ";
+    std::cin >> name;
+    std::cout << "Hello " << name << std::endl;
+}
+
+// a function that ask user to input two numbers
+void twonum(int &num1, int &num2) {
+    std::cout << "Enter first num: ";
+    std::cin >> num1;
+    std::cout << "Enter second num: ";
+    std::cin >> num2;
+}
+
+// a function that add num1 and num 2
+void add(int num1, int num2) {
+    int sum = num1 + num2;
+    std::cout << "The sum of two numbers is: " << sum << std::endl;
+}
+
+// a function that subract num1 and num2
+void sub(int num1, int num2) {
+    int diff = num1 - num2;
+    std::cout << "The difference of two numbers is: " << diff << std::endl;
+}
+
+// a functuon that multiply the num1 and num2
+void mult(int num1, int num2) {
+    int prod = num1 * num2;
+    std::cout << "The product of two numbers is: " << prod << std::endl;
+}
+
+// a function that divide the num1 and num2
+void divi(int num1, int num2) {
+    int quot = num1 / num2; 
+    std::cout << "The quotient of two numbers is: " << quot << std::endl;
+}
+
+// a function that remainder of num1 and num2
+void modu(int num1, int num2) {
+    int mod = num1 % num2; 
+    std::cout << "The modulus of two numbers is: " << mod << std::endl;
+}
+
+int main() {
+    // call all the function in main function to run the code
+    name();
+    
+    
+    int num1, num2;
+    twonum(num1, num2);
+    
+    add(num1, num2);
+    sub(num1, num2);
+    mult(num1, num2);
+    divi(num1, num2);
+    modu(num1, num2);
+    
+    return 0;
+}
